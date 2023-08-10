b2389c50dfb68b71858de1eab94fe376a57b5086
David Lucker <david.l@gojek.com>
2023-08-10 21:33:18 +0700

    can make basic call to open ai api

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
+
+# Imports
+import os
 from langchain.llms import OpenAI
+
+# Setup 
+key = os.environ["OPENAI_API_KEY"]
+llm = OpenAI(temperature=0.9, openai_api_key=key)
+
+# Call API
+out = llm.predict("How are you?")
+print(out)
