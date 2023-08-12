560514f5523f706c0c798cdd655b9b7f9adc5707
David Lucker <david.l@gojek.com>
2023-08-12 12:03:44 +0700

    switching to ChatOpenAI class

--- bot.py
+++ bot.py
@@ -2,12 +2,27 @@
 
 # Imports
 import os
-from langchain.llms import OpenAI
+# from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.prompts.chat import SystemMessage, HumanMessagePromptTemplate
 
 # Setup 
 key = os.environ["OPENAI_API_KEY"]
-llm = OpenAI(temperature=0.9, openai_api_key=key)
 
-# Call API
-out = llm.predict("How are you?")
+
+template = ChatPromptTemplate.from_messages(
+    [
+        SystemMessage(
+            content=(
+                "You are a helpful assistant that re-writes the user's text to "
+                "sound more upbeat."
+            )
+        ),
+        HumanMessagePromptTemplate.from_template("{text}"),
+    ]
+)
+
+llm = ChatOpenAI(temperature=0.9)
+out = llm(template.format_messages(text='i hate the weather today.'))
 print(out)
